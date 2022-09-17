dfab1ea420a4eecf44b0b34722efea12cbf77c62
LmOpe <lawalmuhammed44@gmail.com>
2022-09-17 15:04:03 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace DigitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.BackColor = System.Drawing.Color.Black;
+            this.lblTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTime.Font = new System.Drawing.Font("Digital-7 Italic", 71.99999F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblTime.Location = new System.Drawing.Point(129, 46);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(238, 98);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "22:22";
+            this.lblTime.Click += new System.EventHandler(this.lblTime_Click);
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.AutoSize = true;
+            this.lblSecond.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSecond.Font = new System.Drawing.Font("Digital-7 Italic", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecond.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblSecond.Location = new System.Drawing.Point(380, 95);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(53, 40);
+            this.lblSecond.TabIndex = 1;
+            this.lblSecond.Text = "22";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDate.Font = new System.Drawing.Font("Digital-7 Italic", 39.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblDate.Location = new System.Drawing.Point(103, 223);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(262, 56);
+            this.lblDate.TabIndex = 2;
+            this.lblDate.Text = "MAY 29 2001";
+            this.lblDate.Click += new System.EventHandler(this.lblDate_Click);
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDay.Font = new System.Drawing.Font("Digital-7 Italic", 39.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDay.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.lblDay.Location = new System.Drawing.Point(381, 223);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(198, 56);
+            this.lblDay.TabIndex = 3;
+            this.lblDay.Text = "TUESDAY";
+            this.lblDay.Click += new System.EventHandler(this.lblDay_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(700, 369);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
